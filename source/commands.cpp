921ef3e74bc866272ab37a6de27f67d9cdaf5473
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2015-09-21 15:56:12 +0300

    added initial version

---
+++ source/commands.cpp
@@ -0,0 +1,94 @@
+// mbed
+#include "mbed.h"
+#include "ble/BLE.h"
+#include "ble/services/iBeacon.h"
+
+// libraries
+#include "mbed-client-cli/ns_cmdline.h"
+
+// application
+#include "commands.h"
+
+BLE ble;
+
+// prototypes
+int cmd_ifconfig(int argc, char* argv[]);
+int cmd_ifconfig_ble(int argc, char* argv[]);
+int cmd_ifconfig_print(int argc, char* argv[]);
+
+// help function for cmd_print
+char* cmd_print_address(Gap::Address_t addr);
+
+
+#define MAN_IFCONFIG    "ifconfig (<iface>) ([options])\r\n"\
+                    "<iface>                Interface, default: ble0\r\n"\
+                    "init                   Initializa\r\n"\
+                    "                       Without options just print current config\r\n"
+
+void initialize_app_commands(void)
+{
+    cmd_add("ifconfig", cmd_ifconfig, "Configure Network", MAN_IFCONFIG);
+}
+
+char address_tmp[16];
+char* cmd_print_address(Gap::Address_t addr)
+{
+    if(snprintf(address_tmp, 16, "%d:%d:%d:%d:%d:%d", 
+       addr[0], addr[1], addr[2], addr[3], addr[4], addr[5])>0) {
+        return address_tmp;
+    }
+    return "";
+}
+int cmd_ifconfig_print(int argc, char* argv[])
+{
+    // print all current interface configurations
+    
+    /* Read in the MAC address of this peripheral. The corresponding central will be
+     * commanded to co-ordinate with this address. */
+    Gap::Address_t     address;
+    Gap::AddressType_t addressType;
+    ble.gap().getAddress(&addressType, address);
+    if( addressType == Gap::ADDR_TYPE_PUBLIC ) {
+        cmd_printf("Public-Address: %s\r\n", cmd_print_address(address));
+    }
+    
+    uint16_t interval = ble.gap().getAdvertisingParams().getInterval();
+    cmd_printf("Advertising interval: %d\r\n", interval);
+    
+    /* Check that the state is one of the valid values. */
+    Gap::GapState_t state = ble.gap().getState();
+    if (state.connected == 1) cmd_printf("Connected\r\n");
+    if (state.advertising == 1) cmd_printf("Advertising\r\n");
+    //...
+       
+    return CMDLINE_RETCODE_SUCCESS;
+}
+int cmd_ifconfig_ble(int argc, char* argv[])
+{
+    int ival;
+    char *val;
+    int ret = -1;
+    
+    if( argc == 1 ) {
+        return cmd_ifconfig_print(argc, argv);
+    }
+    if( cmd_parameter_index(argc, argv, "init" ) == 1 ) {
+        ret = ble.init();
+    } else if( cmd_parameter_val(argc, argv, "--addr", &val) ){
+        Gap::Address_t address; // @todo convert string to address..
+        ret = ble.gap().setAddress(Gap::ADDR_TYPE_PUBLIC, address);
+    } else if( cmd_parameter_int(argc, argv, "--adv-timeout", &ival)) {
+        ble.gap().setAdvertisingInterval(ival); /* in milliseconds. */
+        ret = 0;
+    } else {
+        return CMDLINE_RETCODE_COMMAND_NOT_IMPLEMENTED;
+    }
+    return ret==0?CMDLINE_RETCODE_SUCCESS:CMDLINE_RETCODE_FAIL;
+}
+
+int cmd_ifconfig(int argc, char* argv[])
+{
+    //default ble0 -interface
+    return cmd_ifconfig_ble(argc, argv);
+}
+
---
+++ source/main.cpp
@@ -0,0 +1,55 @@
+#include "mbed.h"
+#include "ble/BLE.h"
+#include "ble/services/iBeacon.h"
+#include "mbed-client-cli/ns_cmdline.h"
+
+#include "commands.h"
+
+// Prototypes
+void cmd_ready_cb(int retcode);
+
+Serial pc(USBTX, USBRX);
+
+void custom_cmd_response_out(const char* fmt, va_list ap)
+{
+    char buffer[200]= {0};
+    vsnprintf(buffer,200,fmt, ap);
+    pc.printf("%s",buffer);
+}
+
+// serial RX interrupt function
+// there should be buffer to improve performance..
+void cmd_cb(void) 
+{
+	cmd_char_input(pc.getc());
+}
+
+// this function should be inside some event scheduler
+// then cli can be use also with async commands
+// see example with event-loop (/minar): https://github.com/ARMmbed/mbed-client-cliapp/blob/master/source/cmd_commands.c
+void cmd_ready_cb(int retcode)
+{
+    cmd_next( retcode );
+}
+
+void app_start(int, char*[])
+{
+    //configure serial port
+    pc.baud(115200);	// This is default baudrate for our test applications. 230400 is also working, but not 460800. At least with k64f.
+    pc.attach(&cmd_cb);
+    
+    cmd_init( &custom_cmd_response_out );
+    cmd_set_ready_cb( cmd_ready_cb );
+    initialize_app_commands();
+}
+/**
+ * main() is needed only for mbed-classic. mbed OS triggers app_start() automatically.
+ */
+#ifndef YOTTA_CFG_MBED_OS
+int main(void)
+{
+    app_start(0, NULL);
+    
+    return 0;
+}
+#endif
