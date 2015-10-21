1e32c8bbbd140de639ce16c7387882d88d74188e
Vincent Coubard <vincent.coubard@arm.com>
2015-10-21 11:55:35 +0100

    Allow serialization and deserialization of ble_error_t Allow serialization and deserialization of mac address implement getAddress and setAddress into gap commands module.

---
+++ source/BleCommonSerializer.h
@@ -0,0 +1,35 @@
+#ifndef BLE_CLIAPP_BLE_COMMON_SERIALIZER_H_
+#define BLE_CLIAPP_BLE_COMMON_SERIALIZER_H_
+
+#include "Serializer.h"
+#include "ble/blecommon.h"
+
+
+template<>
+struct SerializerDescription<ble_error_t> {
+	typedef ble_error_t type;
+
+	static const ConstArray<ValueToStringMapping<type> > mapping() {
+	    static const ValueToStringMapping<type> map[] = { 
+		    { BLE_ERROR_NONE, "BLE_ERROR_NONE" },
+		    { BLE_ERROR_BUFFER_OVERFLOW, "BLE_ERROR_BUFFER_OVERFLOW" },
+		    { BLE_ERROR_NOT_IMPLEMENTED, "BLE_ERROR_NOT_IMPLEMENTED" },
+		    { BLE_ERROR_PARAM_OUT_OF_RANGE, "BLE_ERROR_PARAM_OUT_OF_RANGE" },
+		    { BLE_ERROR_INVALID_PARAM, "BLE_ERROR_INVALID_PARAM" },
+		    { BLE_STACK_BUSY, "BLE_STACK_BUSY" },
+		    { BLE_ERROR_INVALID_STATE, "BLE_ERROR_INVALID_STATE" },
+		    { BLE_ERROR_NO_MEM, "BLE_ERROR_NO_MEM" },
+		    { BLE_ERROR_OPERATION_NOT_PERMITTED, "BLE_ERROR_OPERATION_NOT_PERMITTED" },
+		    { BLE_ERROR_UNSPECIFIED, "BLE_ERROR_UNSPECIFIED" }
+	    };
+
+	    return makeConstArray(map);
+	}
+
+	static const char* errorMessage() { 
+		return "unknown ble_error_t";
+	}
+};
+
+
+#endif //BLE_CLIAPP_BLE_COMMON_SERIALIZER_H_
