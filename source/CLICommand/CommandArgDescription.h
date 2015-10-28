692e11b93f3830d0092c32e518761a7b72a5f54f
Vincent Coubard <vincent.coubard@arm.com>
2015-10-28 23:26:09 +0000

    Add CommandArgsDescription to the CLI framework, make ConstArray constexpr and add argsDescription mmeber into Command

---
+++ source/CLICommand/CommandArgDescription.h
@@ -0,0 +1,11 @@
+#ifndef BLE_CLIAPP_COMMAND_ARGS_DESCRIPTION_H_
+#define BLE_CLIAPP_COMMAND_ARGS_DESCRIPTION_H_
+
+
+struct CommandArgDescription { 
+	const char* name;
+	const char* desc;
+};
+
+
+#endif //BLE_CLIAPP_COMMAND_ARGS_DESCRIPTION_H_
