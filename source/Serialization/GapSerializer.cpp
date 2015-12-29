a07b914432fd7b71ca33a8e06951e99d2e01e3e0
Vincent Coubard <vincent.coubard@arm.com>
2015-12-29 09:41:36 +0000

    Cleanup GapSerializer:
    - move code from header files to implementation

---
+++ source/Serialization/GapSerializer.cpp
@@ -0,0 +1,71 @@
+#include "GapSerializer.h"
+
+// MAC address serializer/deserializer
+bool macAddressFromString(const char* str, Gap::Address_t& val) {
+    // since nano libc does not properly handle all format flags, we rely on integers for sscanf
+    int tmp[Gap::ADDR_LEN];
+
+    int count = sscanf(str, "%x:%x:%x:%x:%x:%x", &tmp[5], &tmp[4], &tmp[3], &tmp[2], &tmp[1], &tmp[0]);
+
+    if(count == Gap::ADDR_LEN) {
+        for(size_t i = 0; i < Gap::ADDR_LEN; ++i) {
+            val[i] = (uint8_t) tmp[i];
+        }
+        return true;
+    }
+
+    return false;
+}
+
+MacAddressString_t macAddressToString(const Gap::Address_t& src) {
+    MacAddressString_t converted;
+    snprintf(converted.str, sizeof(converted.str), "%02X:%02X:%02X:%02X:%02X:%02X", src[5], src[4], src[3],src[2],src[1],src[0]);
+    return converted;
+}
+
+// TODO : bidirectional way of serialization / deserialization
+
+/// convert Gap::ConnectionParams_t from CLI to structure
+bool connectionParamsFromCLI(const char* str, Gap::ConnectionParams_t& params) {
+    // since nano libc does not properly handle all format flags, we rely on integers for sscanf
+    unsigned int tmp[4];
+
+    int count = sscanf(str, "%u,%u,%u,%u", &tmp[0], &tmp[1], &tmp[2], &tmp[3]);
+
+    if(count == 4) {
+        params.minConnectionInterval = tmp[0];
+        params.maxConnectionInterval = tmp[1];
+        params.slaveLatency = tmp[2];
+        params.connectionSupervisionTimeout = tmp[3];
+        return true;
+    }
+
+    return false;
+}
+
+dynamic::Value connectionParamsToJSON(const Gap::ConnectionParams_t& params) {
+    dynamic::Value res;
+    res["minConnectionInterval"_ss] = (int64_t) params.minConnectionInterval;
+    res["maxConnectionInterval"_ss] = (int64_t) params.maxConnectionInterval;
+    res["slaveLatency"_ss] = (int64_t) params.slaveLatency;
+    res["connectionSupervisionTimeout"_ss] = (int64_t) params.connectionSupervisionTimeout;
+    return res;
+}
+
+// TODO better than that, this has to be generic !!!!
+dynamic::Value txPermittedValuesToJSON(const int8_t* permittedTxPowerValues, size_t permittedTxPowerValuesCount) {
+    dynamic::Value result;
+    for(size_t i = 0; i < permittedTxPowerValuesCount; ++i) {
+        result.push_back((int64_t) permittedTxPowerValues[i]);
+    }
+
+    return result;
+}
+
+dynamic::Value gapStateToJSON(Gap::GapState_t state) {
+    dynamic::Value result;
+    result["advertising"_ss] = state.advertising ? true : false;
+    result["connected"_ss] = state.connected ? true : false;
+    return result;
+}
+
--- source/Serialization/GapSerializer.h
+++ source/Serialization/GapSerializer.h
@@ -70,73 +70,15 @@ struct MacAddressString_t {
 };
 
 // MAC address serializer/deserializer
-static inline bool macAddressFromString(const char* str, Gap::Address_t& val) {
-    // since nano libc does not properly handle all format flags, we rely on integers for sscanf
-    int tmp[Gap::ADDR_LEN];
-
-    int count = sscanf(str, "%x:%x:%x:%x:%x:%x", &tmp[5], &tmp[4], &tmp[3], &tmp[2], &tmp[1], &tmp[0]);
-
-    if(count == Gap::ADDR_LEN) {
-        for(size_t i = 0; i < Gap::ADDR_LEN; ++i) {
-            val[i] = (uint8_t) tmp[i];
-        }
-        return true;
-    }
-
-    return false;
-}
-
-static inline MacAddressString_t macAddressToString(const Gap::Address_t& src) {
-    MacAddressString_t converted;
-    snprintf(converted.str, sizeof(converted.str), "%02X:%02X:%02X:%02X:%02X:%02X", src[5], src[4], src[3],src[2],src[1],src[0]);
-    return converted;
-}
-
-
-// TODO : bidirectional way of serialization / deserialization
+bool macAddressFromString(const char* str, Gap::Address_t& val);
+MacAddressString_t macAddressToString(const Gap::Address_t& src);
 
 /// convert Gap::ConnectionParams_t from CLI to structure
-static inline bool connectionParamsFromCLI(const char* str, Gap::ConnectionParams_t& params) {
-    // since nano libc does not properly handle all format flags, we rely on integers for sscanf
-    unsigned int tmp[4];
+bool connectionParamsFromCLI(const char* str, Gap::ConnectionParams_t& params);
 
-    int count = sscanf(str, "%u,%u,%u,%u", &tmp[0], &tmp[1], &tmp[2], &tmp[3]);
+dynamic::Value connectionParamsToJSON(const Gap::ConnectionParams_t& params);
 
-    if(count == 4) {
-        params.minConnectionInterval = tmp[0];
-        params.maxConnectionInterval = tmp[1];
-        params.slaveLatency = tmp[2];
-        params.connectionSupervisionTimeout = tmp[3];
-        return true;
-    }
-
-    return false;
-}
-
-static inline dynamic::Value connectionParamsToJSON(const Gap::ConnectionParams_t& params) {
-    dynamic::Value res;
-    res["minConnectionInterval"_ss] = (int64_t) params.minConnectionInterval;
-    res["maxConnectionInterval"_ss] = (int64_t) params.maxConnectionInterval;
-    res["slaveLatency"_ss] = (int64_t) params.slaveLatency;
-    res["connectionSupervisionTimeout"_ss] = (int64_t) params.connectionSupervisionTimeout;
-    return res;
-}
-
-// TODO better than that, this has to be generic !!!!
-static inline dynamic::Value txPermittedValuesToJSON(const int8_t* permittedTxPowerValues, size_t permittedTxPowerValuesCount) {
-    dynamic::Value result;
-    for(size_t i = 0; i < permittedTxPowerValuesCount; ++i) {
-        result.push_back((int64_t) permittedTxPowerValues[i]);
-    }
-
-    return result;
-}
-
-static inline dynamic::Value gapStateToJSON(Gap::GapState_t state) {
-    dynamic::Value result;
-    result["advertising"_ss] = state.advertising ? true : false;
-    result["connected"_ss] = state.connected ? true : false;
-    return result;
-}
+dynamic::Value txPermittedValuesToJSON(const int8_t* permittedTxPowerValues, size_t permittedTxPowerValuesCount);
+dynamic::Value gapStateToJSON(Gap::GapState_t state);
 
 #endif //BLE_CLIAPP_GAP_SERIALIZER_H_
