9acfd87294b1f8830eff02d09c851417c3d77606
Vincent Coubard <vincent.coubard@arm.com>
2016-02-05 17:59:53 +0000

    Utility to hijack c++ right access. It is necessary for internal manipulations when building services lazily.

---
+++ source/util/HijackMember.h
@@ -0,0 +1,39 @@
+#ifndef BLE_CLIAPP_UTIL_HIJACK_MEMBER_H_
+#define BLE_CLIAPP_UTIL_HIJACK_MEMBER_H_
+
+/**
+ * @brief provide access to a private class member function or member data
+ * @details It is possible to explicitly instantiate a template class with a
+ * private pointer to member. When such instantiation occur, a friend function
+ * getMember is instantiated. This function will return the pointer to member
+ * used at the instantiation.
+ *
+ * @tparam MemberAccessor Act as a key to resolve the correct overload of getMember
+ * @tparam MemberType The type of the pointer to member that will be hijacked
+ * @tparam Member The pointer to member hijacked
+ */
+template<typename MemberAccessor, typename MemberType, MemberType Member>
+struct hijacked_member_accessor {
+    friend constexpr MemberType getMember(MemberAccessor) {
+        return Member;
+    }
+};
+
+/**
+ * @brief Provide an accessors to a pointer to member, even if this one is private
+ *
+ * @param ACCESSOR name of the accessor to create
+ * @param MEMBER_TYPE type of the pointer to member
+ * @param MEMBER the pointer to member hijacked
+ */
+#define HIJACK_MEMBER(ACCESSOR, MEMBER_TYPE, MEMBER) \
+    struct ACCESSOR##_CLASS__ { \
+            constexpr ACCESSOR##_CLASS__() = default; \
+            friend constexpr MEMBER_TYPE getMember(ACCESSOR##_CLASS__);\
+        };\
+    template struct hijacked_member_accessor<ACCESSOR##_CLASS__, MEMBER_TYPE, MEMBER>; \
+    static const auto ACCESSOR = getMember(ACCESSOR##_CLASS__())
+
+
+
+#endif //BLE_CLIAPP_UTIL_HIJACK_MEMBER_H_
