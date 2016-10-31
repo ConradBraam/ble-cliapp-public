fa2fb1e1881329611e9dbbb934961a6dbd5fafe4
Vincent Coubard <vincent.coubard@arm.com>
2016-10-31 11:22:05 +0000

    Use CommandHelper macros to declare commands for the BLE module.

--- source/Commands/BLECommands.cpp
+++ source/Commands/BLECommands.cpp
@@ -2,6 +2,8 @@
 #include "Serialization/BLECommonSerializer.h"
 #include "ble/FunctionPointerWithContext.h"
 #include "util/AsyncProcedure.h"
+#include "CLICommand/CommandHelper.h"
+
 
 using mbed::util::SharedPointer;
 
@@ -13,17 +15,13 @@ static BLE& ble() {
 }
 
 
-struct ShutdownCommand : public BaseCommand {
-    static const char* name() {
-        return "shutdown";
-    }
-
-    static const char* help() {
-        return "Shutdown the current BLE instance, calling ble related function after this"
-               "call may lead to faillure.";
-    }
-
-    static void handler(const CommandArgs&, const SharedPointer<CommandResponse>& response) {
+DECLARE_CMD(ShutdownCommand) {
+    CMD_NAME("shutdown")
+    CMD_HELP(
+        "Shutdown the current BLE instance, calling ble related function after this"
+        "call may lead to faillure."
+    )
+    CMD_HANDLER(args, response) {
         ble_error_t err = ble().shutdown();
         if(err) {
             response->faillure(err);
@@ -34,17 +32,13 @@ struct ShutdownCommand : public BaseCommand {
 };
 
 
-struct InitCommand : public BaseCommand {
-    static const char* name() {
-        return "init";
-    }
-
-    static const char* help() {
-        return "Initialize the ble API and underlying BLE stack.\r\n"
-        "Be sure to call this function before any other ble API function";
-    }
-
-    static void handler(const CommandArgs&, const SharedPointer<CommandResponse>& response) {
+DECLARE_CMD(InitCommand) {
+    CMD_NAME("init")
+    CMD_HELP(
+        "Initialize the ble API and underlying BLE stack.\r\n"
+        "Be sure to call this function before any other ble API function"
+    )
+    CMD_HANDLER(args, response) {
         if(ble().hasInitialized()) {
             response->success();
             return;
@@ -75,17 +69,13 @@ struct InitCommand : public BaseCommand {
 };
 
 
-struct ResetCommand : public BaseCommand {
-    static const char* name() {
-        return "reset";
-    }
-
-    static const char* help() {
-        return "Reset the ble API and ble stack.\r\n"
-               "This function internaly does a reset and an init";
-    }
-
-    static void handler(const CommandArgs&, const SharedPointer<CommandResponse>& response) {
+DECLARE_CMD(ResetCommand) {
+    CMD_NAME("reset")
+    CMD_HELP(
+        "Reset the ble API and ble stack.\r\n"
+        "This function internaly does a reset and an init"
+    )
+    CMD_HANDLER(args, response) {
         ble_error_t err;
         if(ble().hasInitialized()) {
             err = ble().shutdown();
@@ -105,16 +95,10 @@ struct ResetCommand : public BaseCommand {
 };
 
 
-struct GetVersionCommand : public BaseCommand {
-    static const char* name() {
-        return "getVersion";
-    }
-
-    static const char* help() {
-        return "Return the version of the BLE API.\r\n";
-    }
-
-    static void handler(const CommandArgs&, const SharedPointer<CommandResponse>& response) {
+DECLARE_CMD(GetVersionCommand) {
+    CMD_NAME("getVersion")
+    CMD_HELP("Return the version of the BLE API.\r\n")
+    CMD_HANDLER(args, response) {
         const char* version = ble().getVersion();
 
         if(version) {
